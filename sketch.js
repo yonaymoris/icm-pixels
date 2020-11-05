edc3ae18febfbec1c24845b1c031a718da114e1c
David Currie <dac@10-18-232-213.dynapool.wireless.nyu.edu>
2020-11-04 20:42:30 -0500

    PIXELATE

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
 let video;
 let detector;
 let detections;
+let counter = 0;
 
 function setup() {
-  createCanvas(480, 360);
+  createCanvas(640, 480);
 
   video = createCapture(VIDEO);
   video.size(width, height);
   video.hide();
-
+  pixelDensity(1);
   detector = ml5.objectDetector('cocossd', modelReady);
 }
 
@@ -33,14 +34,13 @@ function gotResults(err, results) {
 }
 
 function draw() {
+  video.loadPixels();
   image(video, 0, 0, width, height);
-
   if (detections) {
     detections.forEach(detection => {
       noStroke();
       fill(255);
       strokeWeight(2);
-      text(detection.label, detection.x + 4, detection.y + 10);
 
       noFill();
       strokeWeight(3);
@@ -49,7 +49,42 @@ function draw() {
       } else {
         stroke(0, 0, 255);
       }
-      rect(detection.x, detection.y, detection.width, detection.height);
+      // rect(
+      //   round(detection.x),
+      //   round(detection.y),
+      //   round(detection.width),
+      //   round(detection.height)
+      // );
+      pixelStuff(
+        round(detection.x),
+        round(detection.y),
+        round(detection.width),
+        round(detection.height),
+        detections.indexOf(detection)
+      );
+      textSize(48);
+      textAlign(CENTER, CENTER);
+      text(
+        detection.label,
+        detection.x + detection.width / 2,
+        detection.y + detection.height / 2
+      );
     });
+  }
+}
+
+function pixelStuff(x, y, w, h, n) {
+  for (let i = x; i < x + w; i += 15) {
+    for (let j = y; j < y + h; j += 15) {
+      let index = (i + j * w) * 4;
+      fill(
+        video.pixels[index],
+        video.pixels[index + 1],
+        video.pixels[index + 2],
+        video.pixels[index + 3]
+      );
+      noStroke();
+      rect(i, j, 15, 15);
+    }
   }
 }
