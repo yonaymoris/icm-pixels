c951f087291ab8f2be37819575978a82340ff0a9
Yona <ngompung@gmail.com>
2021-11-09 02:13:34 -0500

    Update for Body everytwhere and here

--- sketch.js
+++ sketch.js
@@ -3,6 +3,15 @@ let detector;
 let detections;
 let counter = 0;
 
+
+// bouncing ball
+let x = 40;
+let y = 40;
+let xspeed = 4;
+let yspeed = 5;
+
+let r = 18;
+
 function setup() {
   createCanvas(640, 480);
 
@@ -36,6 +45,14 @@ function gotResults(err, results) {
 function draw() {
   video.loadPixels();
   image(video, 0, 0, width, height);
+
+  push();
+  fill("#fff");
+  ellipse(x, y, r*2, r*2);
+  pop();
+  x += xspeed;
+  y += yspeed;
+
   if (detections) {
     detections.forEach(detection => {
       noStroke();
@@ -46,30 +63,50 @@ function draw() {
       strokeWeight(3);
       if (detection.label === 'person') {
         stroke(0, 255, 0);
-      } else {
-        stroke(0, 0, 255);
+
+        rect(
+          round(detection.x),
+          round(detection.y),
+          round(detection.width),
+          round(detection.height)
+        );
+        pixelStuff(
+          round(detection.x),
+          round(detection.y),
+          round(detection.width),
+          round(detection.height),
+          detections.indexOf(detection)
+        );
+        textSize(48);
+        textAlign(CENTER, CENTER);
+        text(
+          detection.label,
+          detection.x + detection.width / 2,
+          detection.y + detection.height / 2
+        );
+        
+        if ((x + r - xspeed < detection.x && x + r > detection.x  &&  y + r > detection.y  && y < detection.y + detection.height) || 
+            (x - xspeed > detection.x + detection.width && x < detection.x + detection.width  &&  y + r > detection.y  && y < detection.y + detection.height)) {
+          xspeed = -xspeed;
+          console.log("It touched x");
+        }
+
+        if ((y + r - yspeed < detection.y && y + r > detection.y && x + r > detection.x && x < detection.x + detection.width) || 
+            (y - yspeed > detection.y + detection.height && y < detection.y + detection.height && x + r > detection.x && x < detection.x + detection.width)) {
+          yspeed = -yspeed;
+          console.log("It touched y");
+          // console.log(detection);
+          // console.log(x, y);
+        }
       }
-      // rect(
-      //   round(detection.x),
-      //   round(detection.y),
-      //   round(detection.width),
-      //   round(detection.height)
-      // );
-      pixelStuff(
-        round(detection.x),
-        round(detection.y),
-        round(detection.width),
-        round(detection.height),
-        detections.indexOf(detection)
-      );
-      textSize(48);
-      textAlign(CENTER, CENTER);
-      text(
-        detection.label,
-        detection.x + detection.width / 2,
-        detection.y + detection.height / 2
-      );
     });
+  }
+
+  if (x > width - r || x < r) {
+    xspeed = -xspeed;
+  }
+  if (y > height - r || y < r) {
+    yspeed = -yspeed;
   }
 }
 
